ef27cdaf46cca7f0dae939690bc6270034f35da5
Philip Samoei <samphilkerich04@gmail.com>
2023-07-11 16:26:58 +0300

    react-hook form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,65 @@
-import React from 'react'
-import  '../css/App.css'
-import { useForm } from 'react-hook-form'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import '../css/App.css';
+import { DevTool } from '@hookform/devtools';
 
 
-const form = useForm();
-const { register} = form;
 
 function Form() {
+  const form = useForm();
+  const { register, control, handleSubmit,formState } = form;
+  const {errors} = formState;
+
+  const onSubmit = (data) => {
+    console.log('Form submitted', data);
+    alert('Form submitted');
+    form.reset();
+
+  };
+
   return (
-    <div>
-        <form>
-            <label htmlFor="username" >Username</label>
-            <input type="text"  id="username" {...register("username")} />
+    <div className='formname'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" {...register('username',{
+          required:{
+            value:true,
+            message:" Username must be provided",
+        },
+      })}
+        />
+        <p className='error'>{errors.username?.message}</p>
 
-            <label htmlFor="email" >Email</label>
-            <input type="email"  id="email" {...register("email")}/>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register('email',
+        {
+          required:{
+            value:true,
+            message:" email must be valid",
+        },
+      })}
+        />
 
-            <label htmlFor="channel" >Channel</label>
-            <input type="channel"  id="channel"{...register("channel")} />
+       
+        <p className='error'>{errors.email?.message}</p>
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register('channel',
+        {
+          required:{
+            value:true,
+            message:" Channel must be provided",
+        },
+      })}
+        />
 
-            <button type="submit">Submit</button>
-        </form>
+
+        <p className='error'>{errors.channel?.message}</p>
+
+        <button type="submit">Submit</button>
+      </form>
+      <DevTool control={control} />
     </div>
-  )
+  );
 }
 
-export default Form
-
+export default Form;
